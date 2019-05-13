0f61540e2308fe0dc64752454f2aef0eed2d6366
Miledh <miledhlz@hotmail.com>
2019-05-13 22:18:24 +0800

    file, week_7_saddle_*.c edit pre_set indexes

--- homework/week_7_saddle_point_.c
+++ homework/week_7_saddle_point_.c
@@ -6,88 +6,89 @@
 （2）尽管有鞍点不存在的情况，但是如果首先预设鞍点不存在的话，
 （2-2）反而增加了代码量，因为需要考虑特殊情况；
 （3）算法本身不复杂，题目说得很明确了，某行的最大的值且该列的最小值。
-*/ 
+*/
 
 #include <stdio.h>
 
 // 定义结构体，用来存放鞍点的行下标、列下标
 struct pt {
-	int col;
-	int row;
+    int col;
+    int row;
 };
 
-struct pt maxInd(int **a, int col, int row);
+struct pt maxInd(int** a, int col, int row);
 
 int main()
 {
-	int n;
-	scanf("%d", &n);
-	int a[n][n];
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			scanf("%d", &a[i][j]);
-		}
-	}
-	
-        // 获取二维数组的各行的首地址
-        int *p[n];
-	for (int i = 0; i < n; i++) {
-		p[i] = a[i];
-	}
+    int n;
+    scanf("%d", &n);
+    int a[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
 
-	struct pt ret;
+    // 获取二维数组的各行的首地址
+    int* p[n];
+    for (int i = 0; i < n; i++) {
+        p[i] = a[i];
+    }
 
-	ret = maxInd(p, n, n);
+    struct pt ret;
 
-	if (ret.col != -1 || ret.row != -1) {
-		printf("%d %d", ret.col, ret.row);
-	} else {
-		printf("NO");
-	}
+    ret = maxInd(p, n, n);
 
-	return 0;
+    if (ret.col != -1 || ret.row != -1) {
+        printf("%d %d", ret.col, ret.row);
+    } else {
+        printf("NO");
+    }
+
+    return 0;
 }
 
 // 查找鞍点，返回鞍点的行下标、列下标，变量类型为结构体pt
-struct pt maxInd(int **a, int col, int row)
+struct pt maxInd(int** a, int col, int row)
 {
-	struct pt maxind;
-	// 预置第一个元素为鞍点，或者说，预设鞍点「存在」
-	maxind.col = 0;
-	maxind.row = 0;
+    struct pt maxind;
 
-        // 刚开始编写程序的时候，预设鞍点「不存在」，带来了2个问题：
-        // （1）对于行数、列数为1的正方形矩阵，程序结果是NO，显然错误;
-        // （2）对于所有元素相同的正方形矩阵，程序结果是NO，仍然错误。
-        // 所以，改为预设鞍点存在，且置为第一个元素，上述问题消除。
-        // 同时，因为有后续代码的对比，如果不是鞍点的话，仍然有正确结果。
+    // 刚开始编写程序的时候，预设鞍点「不存在」，带来了2个问题：
+    // （1）对于行数、列数为1的正方形矩阵，程序结果是NO，显然错误;
+    // （2）对于所有元素相同的正方形矩阵，程序结果是NO，仍然错误。
+    // 所以，改为预设鞍点存在，且置为第一个元素，上述问题消除。
+    // 同时，因为有后续代码的对比，如果不是鞍点的话，仍然有正确结果。
 
-	for (int i = 0; i < col; i++) {
-		// （1）先找出行的最大值
-		int max_c = a[i][0];
-		for (int j = 0; j < row; j++) {
-			if (max_c < a[i][j]) {
-				max_c = a[i][j];
-				maxind.col = i;
-				maxind.row = j;
-			}
-		}
-		// 检查行的最大值是不是该列的最小值
-		for (int k = 0; k < col; k++) {
-			if (a[maxind.col][maxind.row] > a[k][maxind.row]) {
-				maxind.col = -1;
-				maxind.row = -1;
-				break;
-			}
-		}
-		// 找到了鞍点，只要找到第一个鞍点就可以结束查找了，
-		// 即使后面还有鞍点也不再查找——题目保证不会有多个鞍点的意思也在于此。
+    for (int i = 0; i < col; i++) {
+        // 预置某行的第一个元素为鞍点，或者说，预设鞍点「存在」
+        maxind.col = i;
+        maxind.row = 0;
+        int max_c = a[i][0];
+       
+        // （1）先找出行的最大值
+        for (int j = 0; j < row; j++) {
+            if (max_c < a[i][j]) {
+                max_c = a[i][j];
+                maxind.col = i;
+                maxind.row = j;
+            }
+        }
+        // 检查行的最大值是不是该列的最小值
+        for (int k = 0; k < col; k++) {
+            if (a[maxind.col][maxind.row] > a[k][maxind.row]) {
+                maxind.col = -1;
+                maxind.row = -1;
+                break;
+            }
+        }
+        // 找到了鞍点，只要找到第一个鞍点就可以结束查找了，
+        // 即使后面还有鞍点也不再查找——题目保证不会有多个鞍点的意思也在于此。
         if (maxind.col != -1 || maxind.row != -1) {
-			break;
-		}
-	}
+            break;
+        }
+    }
 
-	return maxind;
+    return maxind;
 }
 
 /*
@@ -118,13 +119,13 @@ NO
 
 输入样例：
 
-4 
+4
 
-1 7 4 1 
+1 7 4 1
 
-4 8 3 6 
+4 8 3 6
 
-1 6 1 2 
+1 6 1 2
 
 0 7 8 9
 
